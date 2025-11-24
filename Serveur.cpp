0673396b2a18b2a20278ff74ed18358670bbe89d
Matteo Carboni <matteo.carboni@student.hepl.be>
2025-11-24 18:38:24 +0100

    Implement message handling in Serveur.cpp and step 3, 4, 5
    Added message sending and receiving logic in the server.

--- Serveur.cpp
+++ Serveur.cpp
@@ -8,7 +8,8 @@
 
 int idQ;
 int pid1,pid2;
-
+//int msgget(key_t key, int msgflg);
+//ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg);
 int main()
 {
   MESSAGE requete;
@@ -34,16 +35,38 @@ int main()
       perror("Erreur lors de la creation de la file de messages");
       exit(EXIT_FAILURE);
   }
+  else{
+     printf("idQ = %d\n",idQ);
+     exit(0);
+  }
 
   // Attente de connection de 2 clients
   fprintf(stderr,"(SERVEUR) Attente de connection d'un premier client...\n");
   // TO DO (etape 5)
   fprintf(stderr,"(SERVEUR) Attente de connection d'un second client...\n");
   // TO DO (etape 5)
+  
+    while (1) {
+    // Attente d'une requête de type 1
+    if (msgrcv(idQ, &requete, sizeof(MESSAGE) - sizeof(long), 1, 0) == -1) {
+        perror("Erreur msgrcv");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "(SERVEUR) Requete recue de %d : --%s--\n", requete.expediteur, requete.texte);
 
-  //while(1) 
-  {
-    // TO DO (etapes 3, 4 et 5)
+    // Construction de la réponse
+    MESSAGE reponse;
+    reponse.type = requete.expediteur;
+    reponse.expediteur = getpid();
+    strcpy(reponse.texte, "Message bien reçu !");
+
+    // Envoi de la réponse
+    if (msgsnd(idQ, &reponse, sizeof(MESSAGE) - sizeof(long), 0) == -1) {
+        perror("Erreur msgsnd");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "(SERVEUR) Envoi de la reponse à %d\n", reponse.type);
+  }
       // Réception d'un message de type 0 (le premier message disponible)
     if (msgrcv(idQ, &requete, sizeof(MESSAGE) - sizeof(long), 0, 0) == -1) {
         perror("Erreur msgrcv");
@@ -54,9 +77,7 @@ int main()
     fprintf(stderr,"(SERVEUR) Requete recue de %d : --%s--\n",requete.expediteur,requete.texte);
     
     fprintf(stderr,"(SERVEUR) Envoi de la reponse a %d\n",destinataire);
-  }
 }
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///// Handlers de signaux ////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
