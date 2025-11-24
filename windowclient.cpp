554dd4485924052947533acd901a96297a933554
Matteo Carboni <matteo.carboni@student.hepl.be>
2025-11-24 18:39:00 +0100

    Implement signal handling and message sending

--- windowclient.cpp
+++ windowclient.cpp
@@ -13,6 +13,18 @@ extern WindowClient *w;
 
 #include "protocole.h" // contient la cle et la structure d'un message
 
+void handler_SIGUSR1(int sig) {
+    MESSAGE msg;
+    // Lecture du message dont le type = pid du client (getpid())
+    if (msgrcv(idQ, &msg, sizeof(MESSAGE) - sizeof(long), getpid(), 0) == -1) {
+        perror("Erreur msgrcv dans handler SIGUSR1");
+        return;
+    }
+    if (w) {
+        w->setRecu(msg.texte);  // Affiche le message dans l’UI
+    }
+}
+
 extern char nomClient[40];
 int idQ; // identifiant de la file de message
 
@@ -26,13 +38,46 @@ WindowClient::WindowClient(QWidget *parent):QMainWindow(parent),ui(new Ui::Windo
 
   // Recuperation de l'identifiant de la file de messages
   fprintf(stderr,"(CLIENT %s %d) Recuperation de l'id de la file de messages\n",nomClient,getpid());
-  // TO DO (etape 2)
+
+  //  (etape 2)
+
+  idQ = msgget(CLE, 0);  // Ouvrir la file existante
+  if (idQ == -1) {
+      perror("Erreur récupération idQ");
+      exit(EXIT_FAILURE);
+  } else {
+      fprintf(stderr, "(CLIENT %s) idQ récupéré = %d\n", nomClient, idQ);
+  }
 
   // Envoi d'une requete d'identification
-  // TO DO (etape 5)
+
+  //  (etape 5)
+
+  MESSAGE msgId;
+  msgId.type = 2;            // Type 2 comme convention pour identification
+  msgId.expediteur = getpid();
+  strcpy(msgId.texte, "client_id");
+  if (msgsnd(idQ, &msgId, sizeof(MESSAGE) - sizeof(long), 0) == -1) {
+      perror("Erreur envoi id client");
+      exit(EXIT_FAILURE);
+  }
 
   // Armement du signal SIGUSR1
-  // TO DO (etape 4)
+
+  //  (etape 4)
+
+  struct sigaction sa;
+  sa.sa_handler = [](int) {
+      MESSAGE msg;
+      if (msgrcv(idQ, &msg, sizeof(MESSAGE) - sizeof(long), getpid(), 0) == -1) {
+          perror("Erreur msgrcv dans handler SIGUSR1");
+          return;
+      }
+      w->setRecu(msg.texte);
+  };
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGUSR1, &sa, NULL);
 }
 
 WindowClient::~WindowClient()
@@ -90,9 +135,30 @@ const char* WindowClient::getRecu()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void WindowClient::on_pushButtonEnvoyer_clicked()
 {
-  fprintf(stderr,"Clic sur le bouton Envoyer\n");
-  // TO DO (etapes 2, 3, 4)
+    fprintf(stderr, "Clic sur le bouton Envoyer\n");
+    // Construction du message à envoyer
+    MESSAGE msg;
+    msg.type = 1;
+    msg.expediteur = getpid();
+    strcpy(msg.texte, getAEnvoyer());
+
+    // Envoi du message
+    if (msgsnd(idQ, &msg, sizeof(MESSAGE) - sizeof(long), 0) == -1) {
+        perror("Erreur msgsnd");
+        return;
+    }
+
+    // Lecture immédiate (on récupère le message du même type pour le test)
+    MESSAGE recuMsg;
+    if (msgrcv(idQ, &recuMsg, sizeof(MESSAGE) - sizeof(long), 1, 0) == -1) {
+        perror("Erreur msgrcv");
+        return;
+    }
+
+    // Affichage dans l’UI
+    setRecu(recuMsg.texte);
 }
+
 
 void WindowClient::on_pushButtonQuitter_clicked()
 {
@@ -103,4 +169,6 @@ void WindowClient::on_pushButtonQuitter_clicked()
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///// Handlers de signaux ////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// TO DO (etape 4)
+//  (etape 4)
+
+
